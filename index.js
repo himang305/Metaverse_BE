f2bba985f1af426b99d8bb5f9e484af72bee1cb4
himang305 <himang305@gmail.com>
2022-08-15 00:07:51 +0530

    another commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const filmrare_routes = require("./routes/filmrare_routes");
 
 app.use(express.json());
